27cb8b390fd3d55f8878ac41d0a53a37cd4dfc71
unknown <andreacignoni@hotmail.com>
2023-04-02 21:20:58 +0100

    update

--- plottask.py
+++ plottask.py
@@ -1,6 +1,7 @@
 # Plot Task
-# This diagram should show 1000 values with a mean of 5
-# and standard deviation of 2
+# This diagram should show 1000 values drawn from a normal distribution
+# with a mean of 5 and standard deviation of 2.
+# It also plots the function h(x) = x^3.
 # Author: Andrea Cignoni
 
 import numpy as np
@@ -13,13 +14,12 @@ np.random.seed(42)
 values = np.random.normal(loc=5, scale=2, size=1000)
 
 # Plot the histogram of generated values
-plt.subplot(1, 2, 1)
 plt.hist(values, bins=30, density=True, edgecolor='black')
 plt.title('Normal Distribution with Mean 5 and Standard Deviation 2')
 plt.xlabel('Value')
 plt.ylabel('Frequency')
 
-#Define function h(x)= x^3
+# Create a new figure to plot the function h(x) = x^3
 def h(x):
     return x**3
  
@@ -27,11 +27,9 @@ def h(x):
 x = np.linspace (0, 10, num=100)
 
 # Plot the function h(x) = x^3
-plt.subplot(1, 2, 2)
 plt.plot(x, h(x))
 plt.title('Function h(x)=x^3')
 plt.xlabel('x')
 
 # Show the plots
-plt.tight_layout()
 plt.show()
