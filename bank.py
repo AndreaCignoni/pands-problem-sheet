af2608d945ca789b9d91b47fd966a65be852a5dc
unknown <andreacignoni@hotmail.com>
2023-04-14 18:31:57 +0100

    bank.py formatting output update

--- bank.py
+++ bank.py
@@ -5,8 +5,9 @@
 # Prompting user to input two integers
 number1 = int(input ('Enter amount1(in cent): '))
 number2 = int(input ('Enter amount2(in cent): '))
-# sum of the amounts divided by 100 
-# outputs a flotaing number showing two decimals
-sum = float (number1 + number2)/100
-# the output is concatenates two strings
-print (f"The sum of this is €" + str(sum))
+# sum of the amounts
+sum = (number1 + number2)
+# the output concatenates a string and an integer in decimal format
+print (f"The sum of this is €" + '{:.2f}'.format(sum/100.))
+# In order to obtain the integer in the decimal format I have followed
+# https://thepythonguru.com/python-string-formatting/
