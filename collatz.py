69fd77316000caa4e503f59e812a8f6b4892ffd1
unknown <andreacignoni@hotmail.com>
2023-04-17 16:55:03 +0100

    collatz update

--- collatz.py
+++ collatz.py
@@ -8,13 +8,20 @@
 # requesting user to input a number
 n = int(input('Please enter a positive integer: '))
 def collatz(n):
-    # Using while loop is used to check if the condition is equal to one
-    while(n > 1):
+    # an empty list is taken to store all sequence values
+    lst=[]
+    lst.append(n)
+    # While loop is used to check if the condition is equal to one
+    while(n != 1):
         # If the number is not equal to one the number is checked to see if it is even or odd
-        if(n % 2):
+        if(n %2==0):
             n=n//2
+            lst.append(n)
         # If the result gives an odd number then the amount is multiplies by 3 and one is added
         else:
             n = n * 3 + 1
-    return(n)
-print (collatz(n))
+            # All the values are appended to the list
+            lst.append(n)
+    print (*lst)
+# The list is the Collatz sequence of the given number
+collatz(n)
