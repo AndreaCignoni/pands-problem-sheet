6db542fd97860a67120630ba3998a5421ef27538
unknown <andreacignoni@hotmail.com>
2023-04-18 21:23:25 +0100

    es.py update

--- es.py
+++ es.py
@@ -10,20 +10,15 @@
 # Using sys.argv module to access command line arguments in a list
 import sys
 # Get the filename from the command line arguments
-anneOfGG = sys.argv[1]
+anneOfGreenGables = sys.argv[1]
 
 # Open the file for reading
-with open(anneOfGG, 'r', encoding="utf-8") as f:
+with open("anne-of-green-gables.txt", 'r', encoding="utf-8") as f:
     # Initialize a counter for the number of e's found
-    content = f.read()
     e_count = 0
     # Loop over each line in the file
     for line in f:
         # Loop over each character in the line
-        for char in line:
-            # Check if the character is 'e' (case-insensitive) and increment the counter if it is
-            if char.lower() == 'e':
-                e_count += 1
-
+        e_count += line.count('e')+line.count('E')
 # Output the final count of e's found
 print("E Count: ", e_count)
